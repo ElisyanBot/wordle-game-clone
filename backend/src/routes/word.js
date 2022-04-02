3e67aa0b47a6e81297b30154168b1b1150ceddb0
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-04-02 16:32:31 +0200

    fetching input placeholder

--- backend/src/routes/word.js
+++ backend/src/routes/word.js
@@ -5,12 +5,28 @@ const router = express.Router();
 
 export default router
   .get("/", async (req, res) => {
+    const { wordLength, multiChar } = req.query;
+
     const wordList = await fetchDictionary();
     // return res.send(randomizeWord(wordList))
+    
+    const word = randomizeWord(
+      wordList,
+      parseInt(wordLength),
+      multiChar == "true"
+    );
 
-    const { wordLength, multiChar } = req.query;
-    res
-      .status(200)
-      .send(randomizeWord(wordList, parseInt(wordLength), multiChar == "true"));
+    const arr = tilePlaceholders(word);
+    res.set("Access-Control-Allow-Origin","*")
+    res.status(200).send(arr);
   })
-  .post("/", async (req, res) => {});
+
+
+//bryt ut denna och skapa test på den!!!
+function tilePlaceholders(string) {
+  let arr = [];
+  for (let i = 0; i < string.length; i++) {
+    arr.push([""]);
+  }
+  return arr;
+}
