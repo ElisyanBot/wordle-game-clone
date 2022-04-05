fa4a3e65a63ea7e048f1322813f76e1d7cdf08bc
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-04-05 19:18:09 +0200

    fixed cors problem

--- backend/src/routes/word.js
+++ backend/src/routes/word.js
@@ -1,32 +1,29 @@
 import express from "express";
+import cors from "cors";
 import fetchDictionary from "../js/fetchDictionary.js";
+import tilePlaceholders from "../js/renderInputTile.js";
 import randomizeWord from "../js/randomizeWord.js";
+
 const router = express.Router();
-
-export default router
-  .get("/", async (req, res) => {
-    const { wordLength, multiChar } = req.query;
-
-    const wordList = await fetchDictionary();
-    // return res.send(randomizeWord(wordList))
-    
-    const word = randomizeWord(
-      wordList,
-      parseInt(wordLength),
-      multiChar == "true"
-    );
-
-    const arr = tilePlaceholders(word);
-    res.set("Access-Control-Allow-Origin","*")
-    res.status(200).send(arr);
+router.use(
+  cors({
+    origin: "*",
   })
+);
 
+router.get("/", async (req, res) => {
+  const { wordLength, multiChar } = req.query;
+  const wordList = await fetchDictionary();
+  // return res.send(randomizeWord(wordList))
 
-//bryt ut denna och skapa test på den!!!
-function tilePlaceholders(string) {
-  let arr = [];
-  for (let i = 0; i < string.length; i++) {
-    arr.push([""]);
-  }
-  return arr;
-}
+  const word = randomizeWord(
+    wordList,
+    parseInt(wordLength),
+    multiChar == "true"
+  );
+
+  const arr = tilePlaceholders(word);
+  res.status(200).send(arr);
+});
+
+export default router;
