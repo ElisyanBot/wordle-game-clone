5818831ed532d74fb25bdedb4bdc35f3fb06e325
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-04-05 19:20:16 +0200

    added render input tiles function for userInput

---
+++ backend/src/js/renderInputTile.js
@@ -0,0 +1,8 @@
+//bryt ut denna och skapa test på den!!!
+export default function tilePlaceholders(string) {
+  let arr = [];
+  for (let i = 0; i < string.length; i++) {
+    arr.push([""]);
+  }
+  return arr;
+}
