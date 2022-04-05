d2932162416f1ca78ed6c7f92e16544e61372aa9
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-04-05 19:15:33 +0200

    adde game route

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { engine } from "express-handlebars";
 import highscoreRoute from "./routes/highscores.js";
 import wordsRoute from "./routes/word.js";
+import gamesRoute from "./routes/game.js";
 
 //mongoDb server
 import mongoose from "mongoose";
@@ -26,9 +27,9 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
   //react || informationssidan?
 });
-
 app.use("/highscores", highscoreRoute);
 app.use("/word", wordsRoute);
+app.use("/game", gamesRoute);
 
 app.listen(5080, () => {
   console.log(`Example app listening on port 5080`);
