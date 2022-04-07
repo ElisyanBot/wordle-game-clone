ffd09eddec31982d5c0ad1bd790e22a55f0b1db2
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-04-07 12:09:47 +0200

    added rerender for create, start and end game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,43 @@
 import "./App.css";
-import GameBoard from "./comp/GameBoard.js";
+import { useState } from "react";
+
+import CreateGame from "./comp/CreateGamePage.js";
+import GameBoard from "./comp/inGame/GameBoard.js";
+import SumbitHighscore from "./comp/submitHighscore.js";
 
 function App() {
-  return (
-    <div className="App">
-      <GameBoard />
-    </div>
-  );
+  const [gameObj, setGameObj] = useState();
+  const [startGame, setStartGame] = useState(false);
+  const [endGame, setEndGame] = useState(false);
+  const [wordLength, setWordLength] = useState(2);
+  const [multiChar, setMultiChar] = useState(false);
+
+  const PageDisplay = () => {
+    if (startGame) {
+      return (
+        <GameBoard
+          wordLength={wordLength}
+          multiChar={multiChar}
+          setObj={setGameObj}
+          obj={gameObj}
+        />
+      );
+    }
+
+    if (endGame) {
+      return <SumbitHighscore />;
+    }
+
+    return (
+      <CreateGame
+        setCreateGame={setStartGame}
+        setMultiChar={setMultiChar}
+        setWordLength={setWordLength}
+      />
+    );
+  };
+
+  return <div className="App">{PageDisplay()}</div>;
 }
 
 export default App;
