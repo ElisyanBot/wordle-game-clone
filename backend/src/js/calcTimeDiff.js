9203602c6e8795e284c9de7d92a42f57bffb202f
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-04-10 11:31:01 +0200

    better printed returned time

--- backend/src/js/calcTimeDiff.js
+++ backend/src/js/calcTimeDiff.js
@@ -4,7 +4,7 @@ export default function calcTimeDiff(start, end) {
   const dateFuture = end;
   const dateNow = start;
   
-  let seconds = Math.floor((dateFuture - (dateNow))/1000);
+  let seconds = Math.round((dateFuture - (dateNow))/1000, 1);
   let minutes = Math.floor(seconds/60);
   let hours = Math.floor(minutes/60);
   let days = Math.floor(hours/24);
@@ -13,5 +13,5 @@ export default function calcTimeDiff(start, end) {
   minutes = minutes-(days*24*60)-(hours*60);
   seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
 
-  return `${minutes}.${seconds}`;
+  return `${minutes} min ${seconds} sec`;
 }
